4ef2721aa5c7151db9ac51b3aa4ffc682f3a732c
AmmarAgeeza <ammarfathy516@gmail.com>
2026-02-06 16:33:14 +0200

    add onboarding screen

--- lib/features/onboarding/screens/onboarding_screen.dart
+++ lib/features/onboarding/screens/onboarding_screen.dart
@@ -5,6 +5,70 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Spacer(),
+            //heading Text
+            Text(
+              "You AI Assistant",
+              style: TextStyle(
+                color: Color(0xff3369FF),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 16),
+            //sub title Text
+            Text(
+              """Using this software,you can ask you questions and receive articles using artificial intelligence assistant""",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Color(0xff757575),
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Spacer(),
+            // Image
+            Image.asset("assets/images/onboarding_image.png"),
+            Spacer(),
+            //continue button
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                padding: EdgeInsets.symmetric(horizontal: 0),
+                backgroundColor: Color(0xff3369FF),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => Scaffold()),
+                );
+              },
+              child: ListTile(
+                title: Center(
+                  child: Text(
+                    "Continue",
+                    style: TextStyle(
+                      color: Color(0xffFFFFFF),
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                trailing: Icon(
+                  Icons.arrow_forward_ios,
+                  color: Color(0xffFFFFFF),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
